3f30668e56e035019e8422b678d63100c525694d
giulianoc <giulianocatrambone@gmail.com>
2025-03-28 10:13:12 +0100

    aggiunto icml

--- buildAndInstall.sh
+++ buildAndInstall.sh
@@ -16,6 +16,8 @@ echo "cp catraMMSLib/target/catraMMSLib.jar ../cibortv/libs"
 cp catraMMSLib/target/catraMMSLib.jar ../cibortv/libs
 echo "cp catraMMSLib/target/catraMMSLib.jar ../catrammswebservices/libs"
 cp catraMMSLib/target/catraMMSLib.jar ../catrammswebservices/libs
+echo "cp catraMMSLib/target/catraMMSLib.jar ../icml/libs"
+cp catraMMSLib/target/catraMMSLib.jar ../icml/libs
 
 echo "In case of push: git push https://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@github.com/giulianoc/CatraMMSJavaLib.git"
 
